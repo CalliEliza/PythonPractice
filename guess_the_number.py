4b5349a405fbfd3bea622f9cd4ad74123cb3e506
Callie Brooks <calidesigns42@gmail.com>
2015-06-14 13:34:15 -0500

    Update guess_the_number.py

--- guess_the_number.py
+++ guess_the_number.py
@@ -13,10 +13,12 @@ NumRangeMin1 = 0
 NumRangeMax1 = 100
 NumRangeMax2 = 1000
 Game100 = True
+global guess
 # helper function to start and restart the game
 
 def new_game():
     global secret_number
+    Numguess = 7
     print "New game. Range is from %s to %s" % (NumRangeMin1, NumRangeMax1)
     print "Number of remaining guesses is %s" % (Numguess)
     secret_number = random.randrange(0,100)
@@ -25,13 +27,17 @@ def new_game():
 
 # define event handlers for control panel
 def range100():
+    global Numguess
     global secret_number
+    Numguess = 7
     print "New game. Range is from %s to %s" % (NumRangeMin1, NumRangeMax1)
     print "Number of remaining guesses is %s" % (Numguess)
     secret_number = random.randrange(0,100)
     print "\n"
 
+
 def range1000():
+    global Numguess
     Numguess = 10
     Game100 = False
     print "New game. Range is from %s to %s" % (NumRangeMin1, NumRangeMax2)
@@ -39,14 +45,6 @@ def range1000():
     secret_number = random.randrange(0,1000)
     print "\n"
 
-    
-    
-def checkGame():
-    # checks to see what game is running
-    if (Game100 == True):
-        Numguess = 7
-    elif (Game100 == False):
-        Numguess = 10
         
 def input_guess(guess):
     global Numguess
@@ -57,49 +55,27 @@ def input_guess(guess):
     print "Guess is %s" %(guess)
     
     # if statment for guessing 1-100
-    if (0 < secret_number < 100):
+    if (Game100 == True):
         Numguess = Numguess - 1
-        if (Numguess == 0):
+        if (secret_number == guess):
+            print "Correct!"
+            print "\n"
+            return new_game()
+        elif (Numguess == 0):
             print "Nope, No more guesses!"
             print "\n"
-            Numguess = 7
             return new_game()
-        elif (secret_number < guess and Numguess > 0):
+        elif (secret_number < guess):
             print "Too High!"
             print "Number of remaining guesses is %s" % (Numguess)
             print "\n"
-        elif (secret_number > guess and Numguess > 0):
+        elif (secret_number > guess):
             print "Too Low!"
             print "Number of remaining guesses is %s" % (Numguess)
             print "\n"
-        elif (secret_number == guess and Numguess > 0):
-            print "Correct!"
-            print "\n"
-            Numguess = 7
-            return new_game()
+       
 
-   
-    # if statements for 1-1000
-    elif(0 < secret_number < 1000):
-        Numguess = Numguess - 1
-        if(Numguess == 0):
-            print "No more guesses!"
-            print "\n"
-            Numguess2 = 10
-            return new_game()
-        if (secret_number < guess):
-            print "Too High!"
-            print "Number of remaining guesses is %s" % (Numguess)
-            print "\n"
-        elif (secret_number > guess and Numguess > 0):
-            print "Too Low!"
-            print "Number of remaining guesses is %s" % (Numguess)
-            print "\n"
-        elif (secret_number == guess):
-            print "Correct!"
-            print "\n"
-            Numguess = 10
-            return new_game()
+
         
 
 
@@ -113,4 +89,5 @@ frame.add_input("Guess: ", input_guess, 200)
 
 # call new_game
 new_game()
+
 
